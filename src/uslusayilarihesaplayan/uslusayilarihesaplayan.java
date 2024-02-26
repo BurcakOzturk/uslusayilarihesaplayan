a78009dacd8c8d6453b05dc7922d3c69b26f6d85
Burçak ÖZTÜRK <159647466+BurcakOzturk@users.noreply.github.com>
2024-02-26 17:38:06 +0300

    ilk yayim

---
+++ src/uslusayilarihesaplayan/uslusayilarihesaplayan.java
@@ -0,0 +1,31 @@
+package uslusayilarihesaplayan;
+
+import java.util.Scanner;
+
+public class uslusayilarihesaplayan {
+
+	public static void main(String[] args) {
+		
+		int sayi,us,i,j;
+		long sonuc=1;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Üssü hesaplanacak sayıyı giriniz : ");
+		sayi = input.nextInt();
+		
+		System.out.print("Üs değerini giriniz : ");
+		us = input.nextInt();
+		
+		for(i=0 ; i< us ; i++) {
+			
+			sonuc = sayi * sonuc;		
+			
+		}
+		
+		System.out.println(sayi+"^"+us+" sayisinin değeri : "+sonuc);		
+		
+		
+
+	}
+
+}
